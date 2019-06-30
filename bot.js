e26998c7615092f78fc29385bbe2b617a7bbdd74
sdaawr <45720212+sdaawr@users.noreply.github.com>
2019-06-30 02:59:50 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,49 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("594491787842945055")
+let channel =     client.channels.get("594828826232487981")
 setInterval(function() {
-channel.send(`I Need credits Hh bm hhbm hhbm hhbm cretors by devil I Need credits Hh bm hhbm hhbm hhbm cretors by devil`);
+channel.send(`I've battled demons that won't let me sleep
+Called to the sea but she abandoned me
+But I won't never give up, no, never give up, no, no
+No, I won't never give up, no, never give up, no, no
+And I won't let you get me down
+I'll keep gettin' up when I hit the ground
+Oh, never give up, no, never give up no, no, oh
+I won't let you get me down
+I'll keep gettin' up when I hit the ground
+Oh, never give up, no, never give up no, no, oh
+I'll find my way, find my way home, oh, oh, oh
+I'll find my way, find my way home, oh, oh, oh
+I'll find my way, find my way home, oh, oh, oh
+I'll find my way, find my way home, oh, oh, oh
+Oh yeah, I'm haunted by the distant past
+Called to the skies but she was she overcast
+But I won't never give up, no, never give up, no, no
+No, I won't never give up, no, never give up, no, no
+And I won't let you get me down
+I'll keep gettin' up when I hit the ground
+Oh, never give up, no, never give up no, no, oh
+I won't let you get me down
+I'll keep gettin' up when I hit the ground
+Oh, never give up, no, never give up no, no, oh
+I'll find my way, find my way home, oh, oh, oh
+I'll find my way, find my way home, oh, oh, oh
+I'll find my way, find my way home, oh, oh, oh
+I'll find my way, find my way home, oh, oh, oh
+Never give up, never give up
+Never give up, never give up
+No, no, oooh
+And I won't let you get me down
+I'll keep gettin' up when I hit the ground
+Oh, never give up, no, never give up no, no, oh
+I won't let you get me down
+I'll keep gettin' up when I hit the ground
+Oh, never give up, no, never give up no, no, oh
+I'll find my way, find my way home, oh, oh, oh
+I'll find my way, find my way home, oh, oh, oh
+I'll find my way, find my way home, oh, oh, oh
+I'll find my way, find my way home, oh, oh, oh`);
 }, 30)
 })
 
